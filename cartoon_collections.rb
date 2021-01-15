206bcda9238a6b69f39c466779a013c64d676a45
Wurdsmith <wurdsmithing@gmail.com>
2021-01-14 18:25:17 -0600

    commit tree d26350bd5a38515ab265db75d6db38398163cfa8

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+    array.each_with_index do |name, index|
+      index += 1
+      puts "#{index}. #{name}"
+  end
+end
+
+def summon_captain_planet(array)
+  array.collect {|name| name.capitalize + "!"}
+  
+end
+
+def long_planeteer_calls(array)
+  array.any? {|call| call.length > 4 }
+
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = 0
+  while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
+  end
 end
